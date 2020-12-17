c51eb96604aa179c1bba45e9f14e86d95fb85a27
pramoteth <pramote33150@gmail.com>
2020-12-17 16:07:17 +0700

    เสร็จในคาบเรียน

--- nodejs/Node01/nodeserver.js
+++ nodejs/Node01/nodeserver.js
@@ -1,11 +1,19 @@
 const http = require('http');
 http
-.createServer(function(req, res){
-    console.log("requests มาอีกแล้ววววว");
-    res.write('Hello from node web server');
+.createServer((req,res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<html>
+        <head>
+        </head>
+        <body>
+            <h1>Yeahhh</h1>
+            <span> testing </span>
+        </body>
+        </html>
+        `);
     res.end();
 })
 .listen(80);
-console.log('one')
-console.log('two')
-console.log('three')
+console.log('one');
+console.log('two');
+console.log('three');
